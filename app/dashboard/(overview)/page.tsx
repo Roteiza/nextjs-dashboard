76a6de5b845b8bddf74e1a53d2092c651cd9bfa2
Rodrigo Oteiza <roteiza.dev@outlook.com>
2024-02-25 01:56:34 -0300

    Project completed

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,6 +3,11 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { Suspense } from 'react';
 import { lusitana } from '@/app/ui/fonts';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard'
+}
 
 export default async function Page () {
 
